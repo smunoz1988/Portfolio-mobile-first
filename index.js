ca430129dcba5cb9c06f4875e5557ded31a4d7a1
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-03-07 11:01:38 -0500

    Fix JS linters

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-function openNav() {
+const menuButton = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close_menu');
+const backPort = document.querySelector('.back_port');
+const backAbout = document.querySelector('.back_about');
+const backCon = document.querySelector('.back_con');
+
+menuButton.addEventListener('click', () => {
   document.getElementById('display_menu').style.display = 'block';
   document.getElementById('main').style.filter = 'blur(5px)';
-}
+});
 
-function closeNav() {
+closeMenu.addEventListener('click', () => {
   document.getElementById('display_menu').style.display = 'none';
   document.getElementById('main').style.filter = 'none';
-}
+});
+
+backPort.addEventListener('click', () => {
+  document.getElementById('display_menu').style.display = 'none';
+  document.getElementById('main').style.filter = 'none';
+});
+
+backAbout.addEventListener('click', () => {
+  document.getElementById('display_menu').style.display = 'none';
+  document.getElementById('main').style.filter = 'none';
+});
+
+backCon.addEventListener('click', () => {
+  document.getElementById('display_menu').style.display = 'none';
+  document.getElementById('main').style.filter = 'none';
+});
