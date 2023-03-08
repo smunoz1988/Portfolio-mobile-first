76dfbc4585ee17197d0333fb53ee237769718d10
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-03-08 10:47:15 -0500

    Add dinamic popup on work section, fix styles

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const closeMenu = document.querySelector('.close_menu');
 const backPort = document.querySelector('.back_port');
 const backAbout = document.querySelector('.back_about');
 const backCon = document.querySelector('.back_con');
+const openWork1 = document.querySelector('.open_work1');
+const closeWork1 = document.querySelector('.close_work');
+
+//  Work popup
+
+const workName = document.querySelector('#work_pop_name');
+const prueba = 'Trabajo Uno';
 
 menuButton.addEventListener('click', () => {
   document.getElementById('display_menu').style.display = 'block';
@@ -26,5 +33,18 @@ backAbout.addEventListener('click', () => {
 
 backCon.addEventListener('click', () => {
   document.getElementById('display_menu').style.display = 'none';
+  document.getElementById('main').style.filter = 'none';
+});
+
+/* work popout */
+
+openWork1.addEventListener('click', () => {
+  document.getElementById('display_work').style.display = 'block';
+  document.getElementById('main').style.filter = 'blur(5px)';
+  workName.innerHTML = `${prueba}`;
+});
+
+closeWork1.addEventListener('click', () => {
+  document.getElementById('display_work').style.display = 'none';
   document.getElementById('main').style.filter = 'none';
 });
