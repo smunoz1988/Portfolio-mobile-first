d9212176889bb48513c1a8459d9bac8633c836c9
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-03-08 23:23:41 -0500

    Add dinamic rendering on project section, fix popup styles

--- index.js
+++ index.js
@@ -3,64 +3,6 @@ const closeMenu = document.querySelector('.close_menu');
 const backPort = document.querySelector('.back_port');
 const backAbout = document.querySelector('.back_about');
 const backCon = document.querySelector('.back_con');
-const openWork1 = document.querySelector('.open_work1');
-const openWork2 = document.querySelector('.open_work2');
-const openWork3 = document.querySelector('.open_work3');
-const openWork4 = document.querySelector('.open_work4');
-const closeWorks = document.querySelector('.close_work');
-
-//  Work popup
-
-const workName = document.querySelector('#work_pop_name');
-const workDescription = document.querySelector('.workDetail');
-const featuredImage = document.querySelector('.imageWork');
-const tech1 = document.querySelector('.tech1');
-const tech2 = document.querySelector('.tech2');
-const tech3 = document.querySelector('.tech3');
-const goLive = document.querySelector('.goLive');
-const goSource = document.querySelector('.goSource');
-const year = document.querySelector('.year');
-
-//  Project Data for projects popup
-
-const projects = [
-  {
-    name: 'Project 1',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    image: 'images/work1.png',
-    technologies: ['html', 'css', 'javascript', 'ruby'],
-    liveLink: 'https://www.google.com',
-    sourceLink: 'https://www.github.com',
-    year: '2020',
-  },
-  {
-    name: 'Project 2',
-    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    image: 'images/work2.png',
-    technologies: ['html', 'css', 'javascript', 'ruby'],
-    liveLink: 'https://www.google.com',
-    sourceLink: 'https://www.github.com',
-    year: '2019',
-  },
-  {
-    name: 'Project 3',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    image: 'images/work3.png',
-    technologies: ['html', 'css', 'javascript', 'ruby'],
-    liveLink: 'https://www.google.com',
-    sourceLink: 'https://www.github.com',
-    year: '2018',
-  },
-  {
-    name: 'Project 4',
-    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    image: 'images/work3.png',
-    technologies: ['html', 'css', 'javascript', 'ruby'],
-    liveLink: 'https://www.google.com',
-    sourceLink: 'https://www.github.com',
-    year: '2016',
-  },
-];
 
 menuButton.addEventListener('click', () => {
   document.getElementById('display_menu').style.display = 'block';
@@ -87,65 +29,139 @@ backCon.addEventListener('click', () => {
   document.getElementById('main').style.filter = 'none';
 });
 
-//  work popout
+//  Project Data for projects in Work Section
 
-openWork1.addEventListener('click', () => {
-  document.getElementById('display_work').style.display = 'block';
-  document.getElementById('main').style.filter = 'blur(5px)';
-  workName.innerHTML = `${projects[0].name}`;
-  workDescription.innerHTML = `${projects[0].description}`;
-  featuredImage.setAttribute('src', projects[0].image);
-  tech1.innerHTML = `${projects[0].technologies[0]}`;
-  tech2.innerHTML = `${projects[0].technologies[1]}`;
-  tech3.innerHTML = `${projects[0].technologies[2]}`;
-  goLive.setAttribute('href', projects[0].liveLink);
-  goSource.setAttribute('href', projects[0].sourceLink);
-  year.innerHTML = `${projects[0].year}`;
-});
+const projects = [
+  {
+    name: 'Project 1',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: 'images/work1.png',
+    technologies: ['html', 'css', 'ruby'],
+    liveLink: 'https://www.google.com',
+    sourceLink: 'https://www.github.com',
+    year: '2020',
+    type: 'card oddCard',
+  },
+  {
+    name: 'Project 2',
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    image: 'images/work2.png',
+    technologies: ['html', 'css', 'javascript'],
+    liveLink: 'https://www.google.com',
+    sourceLink: 'https://www.github.com',
+    year: '2019',
+    type: 'card pairCard',
+  },
+  {
+    name: 'Project 3',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: 'images/work3.png',
+    technologies: ['css', 'javascript', 'ruby'],
+    liveLink: 'https://www.google.com',
+    sourceLink: 'https://www.github.com',
+    year: '2018',
+    type: 'card oddCard',
+  },
+  {
+    name: 'Project 4',
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    image: 'images/work3.png',
+    technologies: ['html', 'css', 'ruby'],
+    liveLink: 'https://www.google.com',
+    sourceLink: 'https://www.github.com',
+    year: '2016',
+    type: 'card pairCard',
+  },
+];
 
-openWork2.addEventListener('click', () => {
-  document.getElementById('display_work').style.display = 'block';
-  document.getElementById('main').style.filter = 'blur(5px)';
-  workName.innerHTML = `${projects[1].name}`;
-  workDescription.innerHTML = `${projects[1].description}`;
-  featuredImage.setAttribute('src', projects[1].image);
-  tech1.innerHTML = `${projects[1].technologies[3]}`;
-  tech2.innerHTML = `${projects[1].technologies[2]}`;
-  tech3.innerHTML = `${projects[1].technologies[1]}`;
-  goLive.setAttribute('href', projects[1].liveLink);
-  goSource.setAttribute('href', projects[1].sourceLink);
-  year.innerHTML = `${projects[1].year}`;
-});
+//  Project Section Card creator
 
-openWork3.addEventListener('click', () => {
-  document.getElementById('display_work').style.display = 'block';
-  document.getElementById('main').style.filter = 'blur(5px)';
-  workName.innerHTML = `${projects[2].name}`;
-  workDescription.innerHTML = `${projects[2].description}`;
-  featuredImage.setAttribute('src', projects[2].image);
-  tech1.innerHTML = `${projects[2].technologies[2]}`;
-  tech2.innerHTML = `${projects[2].technologies[0]}`;
-  tech3.innerHTML = `${projects[2].technologies[1]}`;
-  goLive.setAttribute('href', projects[2].liveLink);
-  goSource.setAttribute('href', projects[2].sourceLink);
-  year.innerHTML = `${projects[2].year}`;
-});
+const cardContainer = document.querySelector('#work');
 
-openWork4.addEventListener('click', () => {
-  document.getElementById('display_work').style.display = 'block';
-  document.getElementById('main').style.filter = 'blur(5px)';
-  workName.innerHTML = `${projects[3].name}`;
-  workDescription.innerHTML = `${projects[3].description}`;
-  featuredImage.setAttribute('src', projects[3].image);
-  tech1.innerHTML = `${projects[3].technologies[1]}`;
-  tech2.innerHTML = `${projects[3].technologies[2]}`;
-  tech3.innerHTML = `${projects[3].technologies[3]}`;
-  goLive.setAttribute('href', projects[3].liveLink);
-  goSource.setAttribute('href', projects[3].sourceLink);
-  year.innerHTML = `${projects[3].year}`;
-});
+for (let i in projects) {
+  const card = `
+  <div class="${projects[i].type}">
+  <img class="cardsection" src=${projects[i].image} alt="SnapshootPortfolio">
+  <div class="cardsection">
+      <h4>${projects[i].name}</h4> 
+      <ul class="client">
+          <li class="worktime"><b>CANOPY</b></li>
+          <li class="worktime"><img src="images/Counter.png"></li>
+          <li class="worktime worktitle">Back End Dev</li>
+          <li class="worktime"><img src="images/Counter.png"></li>
+          <li class="worktitle">${projects[i].year}</li>
+      </ul>
+      <p>${projects[i].description}</p>
+      <ul class="client">
+          <li class="worktime tag"><b>${projects[i].technologies[0]}</b></li>
+          <li class="worktime tag"><b>${projects[i].technologies[1]}</b></li>
+          <li class="worktime tag"><b>${projects[i].technologies[2]}</b></li>
+      </ul>
+      <button class="cardButton open_work" type="button">See Project</button>
+  </div>
+</div>
+  `;
+  cardContainer.innerHTML += card;
+}
 
-closeWorks.addEventListener('click', () => {
-  document.getElementById('display_work').style.display = 'none';
-  document.getElementById('main').style.filter = 'none';
+//  Projects popoup
+
+const buttonCall = document.querySelectorAll('.open_work');
+const cardWork = document.querySelector('#display_work');
+
+buttonCall.forEach((buton, index) => {
+  buton.addEventListener('click', () => {
+    const selecButton = projects[index];
+    const addHtml = `
+    <div class="work_overlay">
+    </div>
+    <div class="center_card">
+      <div class="cardPop">
+        <div class="work_pop_title">
+          <h4 id="work_pop_name">${selecButton.name}</h4>
+          <a class="close_work">&times;</a>
+        </div>
+        <ul>
+          <li class="worktime"><b>CANOPY</b></li>
+          <li class="worktime"><img src="images/Counter.png"></li>
+          <li class="worktime worktitle">Back End Dev</li>
+          <li class="worktime"><img src="images/Counter.png"></li>
+          <li class="worktime worktitle year">${selecButton.year}</li>
+        </ul>
+        <div>
+          <img class="imageWork" src=${selecButton.image} alt="SnapshootPortfolio">
+        </div>
+        <div class="workInfoDesktop">
+          <div>
+              <p class="workDetail">${selecButton.description}</p>
+          </div>
+          <div class="techLiveSource">
+              <ul class="client techBox">
+                <li class="worktime tag tech1">${selecButton.technologies[0]}</li>
+                <li class="worktime tag tech2">${selecButton.technologies[1]}</li>
+                <li class="worktime tag tech3">${selecButton.technologies[2]}</li>
+              </ul>
+              <div class="buttonBox">
+                <a class="goLive"  href=${selecButton.liveLink}>
+                  <button class="goButtons" type="button">See Live <img src="images/live.png" alt="Go live"></button>
+                </a>
+                <a class="goSource" href=${selecButton.sourceLink}>
+                  <button class="goButtons" type="button">See Source <img src="images/source.png" alt="Go Source"></button>
+                </a>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+    cardWork.innerHTML = addHtml;
+    document.getElementById('display_work').style.display = 'block';
+    document.getElementById('main').style.filter = 'blur(5px)';
+
+    const closeWorks = document.querySelector('.close_work');
+    closeWorks.addEventListener('click', () => {
+      document.getElementById('display_work').style.display = 'none';
+      document.getElementById('main').style.filter = 'none';
+    });
+    });
 });
